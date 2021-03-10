fa460e004754be337756613639119fdf7791088a
Sergey <moshko.2015@mail.ru>
2021-03-10 15:53:00 +0300

    Second commit, 33lesson task

--- project/js/script.js
+++ project/js/script.js
@@ -10,41 +10,113 @@
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
 
-5) Добавить нумерацию выведенных фильмов */
+5) Добавить нумерацию выведенных фильмов 
 
-'use strict';
+--------------------------------------------------------------------------------------------
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-const reklamas = document.querySelectorAll('.promo__adv img');
-reklamas.forEach(item => {
-    item.remove();
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
+
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "А-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const reklamas = document.querySelectorAll('.promo__adv img'),
+        genre = document.querySelector('.promo__genre'),
+        back = document.querySelector('.promo__bg'),
+        watchedFilms = document.querySelector('.promo__interactive-list'),
+        addFilmForm = document.querySelector('.add'),
+        addFilmInput = addFilmForm.querySelector('.adding__input'),
+        chkBx = addFilmForm.querySelector('[type="checkbox"]');
+
+    const delRekl = (reklBlock) => {
+        reklBlock.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChange = (genre, back) => {
+        genre.textContent = "ДРАММА";
+        back.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    const createMovieList = (arr, element) => {
+        element.innerHTML = "";
+
+        sortArr(arr);
+
+        arr.forEach((item, i) => {
+            element.insertAdjacentHTML("beforeend",
+                `<li class = "promo__interactive-item">
+        ${i + 1}. ${item} 
+        <div class="delete"></div> 
+        </li>`);
+        });
+
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                arr.splice(i, 1);
+
+                createMovieList(arr,element);
+            });
+        });
+    };
+
+    delRekl(reklamas);
+    makeChange(genre, back);
+    createMovieList(movieDB.movies, watchedFilms);
+
+    addFilmForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        let inpVal = addFilmInput.value;
+
+        if (inpVal) {
+
+            const maxNumSimbols = 21;
+
+            if (inpVal.length > maxNumSimbols) {
+                inpVal = `${inpVal.substring(0, maxNumSimbols)}...`;
+            }
+
+            if (chkBx.checked) {
+                console.log("Добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(inpVal);
+
+            createMovieList(movieDB.movies, watchedFilms);
+        }
+
+        event.target.reset();
+    });
 });
-
-const genre = document.querySelector('.promo__genre');
-genre.textContent = "ДРАММА";
-
-const back = document.querySelector('.promo__bg');
-back.style.backgroundImage = "url('img/bg.jpg')";
-
-const watchedFilms = document.querySelector('.promo__interactive-list');
-watchedFilms.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((item, i) => {
-    watchedFilms.insertAdjacentHTML("beforeend",
-    `<li class = "promo__interactive-item">
-    ${i + 1}. ${item} 
-    <div class="delete"></div> 
-    </li>`);
-});
-
